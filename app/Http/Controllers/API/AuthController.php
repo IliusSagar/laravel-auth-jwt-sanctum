cfbdb1d3aff8f8a8db9fc556cd0d69456b06771c
IliusSagar <sagar01830@gmail.com>
2023-11-20 00:12:27 +0600

    controller, api & jwt token create

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Cookie;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $user = User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password'))
+        ]);
+
+        return response(['message' => 'User registered successfully']);
+    }
+
+    public function login(Request $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response(['message' => 'Invalid credentials'], 404);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('token')->plainTextToken;
+        $cookie = cookie('jwt', $token, 60 * 24); // Adjust the expiration time as needed
+
+        return response(['message' => 'Success'])->withCookie($cookie);
+    }
+
+    public function user()
+    {
+        return Auth::user();
+    }
+
+    public function logout()
+    {
+        $cookie = Cookie::forget('jwt');
+
+        return response(['message' => 'Logout success'])->withCookie($cookie);
+    }
+}
